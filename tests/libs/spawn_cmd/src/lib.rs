3fa8a38108a4381dcec917c2c80fafe9bdf87e89
joii2020 <joii@cryptape.com>
2024-10-08 10:31:14 +0800

    SpawnCmd using num_derive

--- tests/libs/spawn_cmd/src/lib.rs
+++ tests/libs/spawn_cmd/src/lib.rs
@@ -1,40 +1,35 @@
 #![no_std]
 
+#[macro_use]
+extern crate num_derive;
+
+#[repr(u8)]
+#[derive(FromPrimitive, ToPrimitive)]
 pub enum SpawnCmd {
-    Base,
+    Base = 1,
     EmptyPipe,
     BaseIO1,
     BaseIO2,
     BaseIO3,
     BaseIO4,
 }
+
+use num_traits::{FromPrimitive, ToPrimitive};
+
 impl From<u8> for SpawnCmd {
     fn from(value: u8) -> Self {
-        match value {
-            0 => Self::Base,
-            1 => Self::EmptyPipe,
-            2 => Self::BaseIO1,
-            3 => Self::BaseIO2,
-            4 => Self::BaseIO3,
-            5 => Self::BaseIO4,
-            _ => panic!("unknow value"),
-        }
+        Self::from_u8(value).unwrap()
     }
 }
-impl Into<u8> for SpawnCmd {
-    fn into(self) -> u8 {
-        match self {
-            Self::Base => 0,
-            Self::EmptyPipe => 1,
-            Self::BaseIO1 => 2,
-            Self::BaseIO2 => 3,
-            Self::BaseIO3 => 4,
-            Self::BaseIO4 => 5,
-        }
-    }
-}
+
 impl From<&str> for SpawnCmd {
     fn from(value: &str) -> Self {
-        u8::from_str_radix(value, 10).expect("parse cmd").into()
+        Self::from_u8(u8::from_str_radix(value, 10).expect("parse cmd")).unwrap()
+    }
+}
+
+impl From<SpawnCmd> for u8 {
+    fn from(value: SpawnCmd) -> Self {
+        value.to_u8().unwrap()
     }
 }
